3d6330f1915163cef19f6bb078ac587e9f9acef0
nicolemichelle88 <daugereaux@gmail.com>
2018-10-14 20:45:47 -0500

    Updates

---
+++ precache-manifest.1bfaa3721ed4539dadfa4a32025ba51d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "cb62b8c001ffbf235163",
+    "url": "/react-todo/static/js/main.cb62b8c0.chunk.js"
+  },
+  {
+    "revision": "d377d35b8f5fe28b4d54",
+    "url": "/react-todo/static/js/1.d377d35b.chunk.js"
+  },
+  {
+    "revision": "cb62b8c001ffbf235163",
+    "url": "/react-todo/static/css/main.44a13f87.chunk.css"
+  },
+  {
+    "revision": "dc1b0ff94f6c0982bcf7adf6ae9cff0b",
+    "url": "/react-todo/index.html"
+  }
+];
